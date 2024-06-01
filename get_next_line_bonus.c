23ef9fc7858aa1465098ddf5c11c7c194f6d19f1
Hugo Chereau <hucherea@paul-f4Br6s3.clusters.42paris.fr>
2024-06-01 15:23:16 +0200

    finish bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hucherea <hucherea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 12:14:28 by imback            #+#    #+#             */
+/*   Updated: 2024/06/01 14:22:07 by hucherea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*get_line_from_buffer(char *line, char *buffer, int bytes_read,
+	int fd)
+{
+	int	i_new_line;
+
+	i_new_line = find_char(buffer, '\n');
+	while (buffer[i_new_line] != '\n' && bytes_read > 0)
+	{
+		line = ft_strnjoin(line, buffer, BUFFER_SIZE);
+		if (line == NULL)
+		{
+			return (NULL);
+		}
+		ft_bzero(buffer, BUFFER_SIZE);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(line);
+			return (NULL);
+		}
+		i_new_line = find_char(buffer, '\n');
+	}
+	return (line);
+}
+
+static char	*get_line(char *buffer, char *line, int fd, int bytes_read)
+{
+	int	i_new_line;
+
+	line = get_line_from_buffer(line, buffer, bytes_read, fd);
+	i_new_line = find_char(buffer, '\n');
+	if (buffer[i_new_line] == '\n')
+	{
+		line = ft_strnjoin(line, buffer, i_new_line + 1);
+		if (line == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (line);
+}
+
+static char	*fill_line(char *buffer, char *line, char *rest, int fd)
+{
+	int	bytes_read;
+	int	index_rest;
+
+	index_rest = find_char(rest, '\n');
+	if (rest[0] != '\0')
+	{
+		line = ft_strnjoin(line, rest, index_rest + 1);
+		if (rest[index_rest] == '\n')
+		{
+			ft_strlcpy(rest, rest + index_rest + 1,
+				ft_strlen(rest) - index_rest);
+			return (line);
+		}
+		else
+			ft_bzero(rest, BUFFER_SIZE);
+	}
+	ft_bzero(buffer, BUFFER_SIZE + 1);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	line = get_line(buffer, line, fd, bytes_read);
+	return (line);
+}
+
+static void	fill_rest(char *buffer, char *rest)
+{
+	const int	index = find_char(buffer, '\n');
+
+	ft_bzero(rest, BUFFER_SIZE);
+	ft_strlcpy(rest, buffer + index + 1, ft_strlen(buffer + index));
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	rest[OPEN_MAX][BUFFER_SIZE + 1];
+	char		*line;
+
+	line = NULL;
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		return (NULL);
+	if (fd >= 0 && fd <= OPEN_MAX)
+	{
+		ft_bzero(buffer, BUFFER_SIZE + 1);
+		line = fill_line(buffer, line, rest[fd], fd);
+		if (rest[fd][0] == '\0' && line != NULL)
+		{
+			fill_rest(buffer, rest[fd]);
+		}
+	}
+	free(buffer);
+	return (line);
+}
+
+// #include <fcntl.h>
+// #include <string.h>
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+// 	fd = open("test.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	while (line)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	close(fd);
+// }
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hucherea <hucherea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 12:15:12 by imback            #+#    #+#             */
+/*   Updated: 2024/05/30 10:11:34 by hucherea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		++len;
+	return (len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (size > 0)
+	{
+		i = 0;
+		while (i < size - 1 && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			++i;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
+
+char	*ft_strnjoin(char *s1, char const *s2, const size_t size)
+{
+	char	*join;
+	size_t	size1;
+	size_t	size2;
+
+	size1 = 0;
+	size2 = 0;
+	if (s1 != NULL)
+		size1 = ft_strlen(s1);
+	if (s2 != NULL)
+		size2 = ft_strlen(s2);
+	if (size2 > size)
+		size2 = size;
+	join = (char *)malloc((size1 + size2 + 1) * sizeof(char));
+	if (join != NULL)
+	{
+		if (s1 != NULL)
+			ft_strlcpy(join, s1, size1 + 1);
+		if (s2 != NULL)
+			ft_strlcpy(join + size1, s2, size2 + 1);
+	}
+	free(s1);
+	return (join);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((uint8_t *)s)[i] = (uint8_t) '\0';
+		++i;
+	}
+}
+
+int	find_char(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	c = (char)c;
+	while (s[i] != c && s[i] != '\0')
+	{
+		++i;
+	}
+	return (i);
+}
